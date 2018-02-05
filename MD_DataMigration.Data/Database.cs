79ae645f9eb399c5d443594ba755253d12a455fe
younggugchoo <30790629+younggugchoo@users.noreply.github.com>
2018-02-05 23:05:54 +0900

    초기 커밋.

---
+++ MD_DataMigration.Data/Database.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MD_DataMigration.Data
+{
+    public abstract class Database
+    {
+        public string connectionString;
+
+        DbProviderFactories
+    }
+}
---
+++ MD_DataMigration.Data/DatabaseFactory.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Configuration;
+using System.Data.Common;
+
+
+namespace MD_DataMigration.Data
+{
+    public class DatabaseFactory
+    {
+        private DbConnection connection = null;
+
+        public DatabaseFactory(string conn)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["MariaDB"];
+            var provider = connectionString.ProviderName;
+            var factory = DbProviderFactories.GetFactory(provider);
+            connection = factory.CreateConnection();
+            connection.ConnectionString = connectionString.ConnectionString;
+            connection.Open();
+        }
+
+        public DbCommand CraeteCommand()
+        {
+            return connection.CreateCommand();
+        }
+
+        public DbDataReader ExecuteReader(string commandText, System.Data.CommandType commandType, params string [] param)
+        {
+            DbCommand dbCommand = CraeteCommand();
+            dbCommand.CommandText = commandText;
+            dbCommand.CommandType = commandType;
+
+            return dbCommand.ExecuteReader();
+        }
+
+        public Object ExecuteScalar(string commandText, DbParameter[] param)
+        {
+            DbCommand dbCommand = CraeteCommand();
+            dbCommand.CommandType = System.Data.CommandType.Text;
+            dbCommand.CommandText = commandText;
+            
+
+            return dbCommand.ExecuteScalar();
+        }
+
+        public int ExecuteNonQuery(string commandText, params string[] param)
+        {
+            DbCommand dbCommand = CraeteCommand();
+            dbCommand.CommandType = System.Data.CommandType.Text;
+            dbCommand.CommandText = commandText;
+            
+            return dbCommand.ExecuteNonQuery();
+        }
+    }
+}
